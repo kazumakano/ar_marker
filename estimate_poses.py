907c73f92cf3f4670f69d256db6d38b918877bf1
kazumakano <kazumakano@outlook.jp>
2023-04-25 19:19:58 +0900

    add estimate_poses.py

--- estimate_poses.py
+++ estimate_poses.py
@@ -1,29 +1,18 @@
 import time
 import cv2
+import numpy as np
 from cv2 import aruco
 import script.utility as util
 
 
-def detect_markers_from_img(dict_idx: int, file: str) -> None:
-    img = cv2.imread(file)
-
-    corners, ids = aruco.detectMarkers(img, aruco.getPredefinedDictionary(dict_idx))[:2]
-    print(f"{len(corners)} markers were detected")
-
-    aruco.drawDetectedMarkers(img, corners)
-    util.draw_ids(corners, ids, img)
-
-    cv2.imshow("image", img)
-
-    print("press any key to exit")
-    cv2.waitKey()
-
-    cv2.destroyAllWindows()
-
-def detect_markers_from_vid(dict_idx: int, file: str) -> None:
+def estim_poses_from_img(cam_file: str, dict_idx: int, marker_len: float, file: str) -> None:
     pass
 
-def detect_markers_on_stream(dict_idx: int, uri: str) -> None:
+def estim_poses_from_vid(cam_file: str, dict_idx: int, marker_len: float, file: str) -> None:
+    pass
+
+def estim_poses_on_stream(cam_file: str, dict_idx: int, marker_len: float, uri: str) -> None:
+    cam_mat, cam_dist_coef = util.load_cam_data(cam_file)
     cap = cv2.VideoCapture(filename=uri)
     prof_dict = aruco.getPredefinedDictionary(dict_idx)
 
@@ -32,6 +21,7 @@ def detect_markers_on_stream(dict_idx: int, uri: str) -> None:
     while True:
         ret, img = cap.read()
         if not ret:
+            # reconnect
             cap.release()
             time.sleep(1)
             cap = cv2.VideoCapture(filename=uri)
@@ -39,8 +29,8 @@ def detect_markers_on_stream(dict_idx: int, uri: str) -> None:
 
         corners, ids = aruco.detectMarkers(img, prof_dict)[:2]
 
-        aruco.drawDetectedMarkers(img, corners)
         util.draw_ids(corners, ids, img)
+        util.draw_poses(cam_mat, cam_dist_coef, img, *aruco.estimatePoseSingleMarkers(corners, marker_len, cam_mat, cam_dist_coef)[:2])
 
         cv2.imshow("stream", img)
 
@@ -55,6 +45,8 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cam_file", required=True, help="specify camera calibration file", metavar="PATH_TO_CAM_FILE")
+    parser.add_argument("-l", "--len", type=float, required=True, help="specify marker length", metavar="LEN")
     parser.add_argument("-d", "--dict", default=aruco.DICT_APRILTAG_16h5, type=int, help="specify profile dictionary index", metavar="IDX")
     parser.add_argument("-i", "--img_file", help="specify image file", metavar="PATH_TO_IMG_FILE")
     parser.add_argument("-s", "--stream", help="specify video stream", metavar="URI")
@@ -62,8 +54,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.img_file is not None:
-        detect_markers_from_img(args.dict, args.img_file)
+        estim_poses_from_img(args.dict, args.img_file)
     elif args.stream is not None:
-        detect_markers_on_stream(args.dict, args.stream)
+        estim_poses_on_stream(args.cam_file, args.dict, args.len, args.stream)
     elif args.vid_file is not None:
-        detect_markers_from_vid(args.dict, args.vid_file)
+        estim_poses_from_vid(args.dict, args.vid_file)
--- script/utility.py
+++ script/utility.py
@@ -1,9 +1,19 @@
 import cv2
 import numpy as np
-from cv2 import aruco
+import yaml
 
 
-def draw_detected_markers(corners: tuple[np.ndarray, ...], ids: np.ndarray, img: np.ndarray) -> None:
-    aruco.drawDetectedMarkers(img, corners)
+def draw_ids(corners: tuple[np.ndarray, ...], ids: np.ndarray, img: np.ndarray) -> None:
     for i in range(len(corners)):
         cv2.putText(img, str(ids[i, 0]), (int(corners[i][0, :, 0].mean()), int(corners[i][0, :, 1].min()) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+
+def draw_poses(cam_mat: np.ndarray, cam_dist_coef: np.ndarray, img: np.ndarray, rvecs: np.ndarray | None, tvecs: np.ndarray | None) -> None:
+    if rvecs is not None and tvecs is not None:
+        for i in range(len(rvecs)):
+            cv2.drawFrameAxes(img, cam_mat, cam_dist_coef, rvecs[i], tvecs[i], 0.1)
+
+def load_cam_data(file: str) -> tuple[np.ndarray, np.ndarray]:
+    with open(file) as f:
+        data = yaml.safe_load(f)
+
+    return np.array(data["camera_matrix"]["data"], dtype=np.float32).reshape(3, 3), np.array(data["distortion_coefficients"]["data"], dtype=np.float32)
