5a899ecd7a450571ac7e4de77634e44e2cbfd42f
kazumakano <kazumakano@outlook.jp>
2023-05-18 20:48:42 +0900

    apply exporting result

--- detect_markers.py
+++ detect_markers.py
@@ -6,7 +6,7 @@ import script.utility as util
 
 FPS = 5
 
-def detect_markers_from_img(dict_idx: int, file: str) -> None:
+def detect_markers_from_img(dict_idx: int, file: str, export: bool = False) -> None:
     img = cv2.imread(file)
 
     corners, ids = aruco.detectMarkers(img, aruco.getPredefinedDictionary(dict_idx))[:2]
@@ -20,13 +20,16 @@ def detect_markers_from_img(dict_idx: int, file: str) -> None:
     print("press any key to exit")
     cv2.waitKey()
 
+    if export:
+        cv2.imwrite(path.join(path.dirname(__file__), "result/", path.basename(file)), img)
+
     cv2.destroyAllWindows()
 
-def detect_markers_from_vid(dict_idx: int, file: str, record: bool = False, start: float = 0) -> None:
+def detect_markers_from_vid(dict_idx: int, file: str, export: bool = False, start: float = 0) -> None:
     cap = cv2.VideoCapture(filename=file)
     cap.set(cv2.CAP_PROP_POS_MSEC, 1000 * start)
     prof_dict = aruco.getPredefinedDictionary(dict_idx)
-    if record:
+    if export:
         recorder = cv2.VideoWriter(path.join(path.dirname(__file__), "result/", path.basename(file)), cv2.VideoWriter_fourcc(*"mp4v"), FPS, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
     print("press any key to exit")
@@ -42,7 +45,7 @@ def detect_markers_from_vid(dict_idx: int, file: str, record: bool = False, star
         util.draw_ids(corners, ids, img)
 
         cv2.imshow("video", img)
-        if record:
+        if export:
             recorder.write(img)
 
         key = cv2.waitKey(delay=1)
@@ -50,15 +53,15 @@ def detect_markers_from_vid(dict_idx: int, file: str, record: bool = False, star
             break
 
     cap.release()
-    if record:
+    if export:
         recorder.release()
     cv2.destroyAllWindows()
 
-def detect_markers_on_stream(dict_idx: int, uri: str, record: bool = False) -> None:
+def detect_markers_on_stream(dict_idx: int, uri: str, export: bool = False) -> None:
     cap = cv2.VideoCapture(filename=uri)
     prof_dict = aruco.getPredefinedDictionary(dict_idx)
-    if record:
-        recorder = cv2.VideoWriter(path.join(path.dirname(__file__), "result/stream"), cv2.VideoWriter_fourcc(*"mp4v"), FPS, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    if export:
+        recorder = cv2.VideoWriter(path.join(path.dirname(__file__), "result/stream.mkv"), cv2.VideoWriter_fourcc(*"mp4v"), FPS, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
     print("press any key to exit")
 
@@ -76,7 +79,7 @@ def detect_markers_on_stream(dict_idx: int, uri: str, record: bool = False) -> N
         util.draw_ids(corners, ids, img)
 
         cv2.imshow("stream", img)
-        if record:
+        if export:
             recorder.write(img)
 
         key = cv2.waitKey(delay=1)
@@ -84,7 +87,7 @@ def detect_markers_on_stream(dict_idx: int, uri: str, record: bool = False) -> N
             break
 
     cap.release()
-    if record:
+    if export:
         recorder.release()
     cv2.destroyAllWindows()
 
@@ -93,16 +96,16 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dict", default=aruco.DICT_APRILTAG_16h5, type=int, help="specify profile dictionary index", metavar="IDX")
+    parser.add_argument("-e", "--export", action="store_true", help="export result")
     parser.add_argument("-i", "--img_file", help="specify image file", metavar="PATH_TO_IMG_FILE")
     parser.add_argument("-s", "--stream", help="specify video stream", metavar="URI")
     parser.add_argument("-v", "--vid_file", help="specify video file", metavar="PATH_TO_VID_FILE")
-    parser.add_argument("-r", "--record", action="store_true", help="record result")
     parser.add_argument("--start", default=0, type=float, help="specify time to start video", metavar="TIME")
     args = parser.parse_args()
 
     if args.img_file is not None:
-        detect_markers_from_img(args.dict, args.img_file)
+        detect_markers_from_img(args.dict, args.img_file, args.export)
     elif args.stream is not None:
-        detect_markers_on_stream(args.dict, args.stream, args.record)
+        detect_markers_on_stream(args.dict, args.stream, args.export)
     elif args.vid_file is not None:
-        detect_markers_from_vid(args.dict, args.vid_file, args.record, args.start)
+        detect_markers_from_vid(args.dict, args.vid_file, args.export, args.start)
